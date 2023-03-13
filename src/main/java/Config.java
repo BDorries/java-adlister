149cf6213a89b19518af4a27999c8b0a4a73ae99
Billie <billie.dorries28@gmail.com>
2023-03-13 14:23:25 -0500

    jdbc

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://studentdb.fulgentcorp.com/ymir_billie?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+    public String getUser() {
+        return "ymir_billie";
+    }
+    public String getPassword() {
+        return "pQABEfM0mDrHxeP";
+    }
+}
